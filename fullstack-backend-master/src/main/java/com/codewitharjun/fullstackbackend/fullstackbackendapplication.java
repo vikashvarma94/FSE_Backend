1bd9b6d36bbeb278f5da566ebae2f205adf80e77
vikashvarma94 <Haritha@98>
2023-11-18 23:37:10 +0530

    18/11 commit

--- fullstack-backend-master/src/main/java/com/codewitharjun/fullstackbackend/fullstackbackendapplication.java
+++ fullstack-backend-master/src/main/java/com/codewitharjun/fullstackbackend/fullstackbackendapplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FullstackBackendApplication {
+public class fullstackbackendapplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FullstackBackendApplication.class, args);
+		SpringApplication.run(fullstackbackendapplication.class, args);
 	}
 
 }
+
