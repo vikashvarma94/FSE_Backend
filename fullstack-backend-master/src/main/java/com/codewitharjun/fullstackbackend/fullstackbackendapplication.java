4af2d6c02f3cb720c37615ac79fdb3f0974336b5
vikashvarma94 <Haritha@98>
2023-12-25 22:49:42 +0530

    25/12 - updated to mysql docker container

--- fullstack-backend-master/src/main/java/com/codewitharjun/fullstackbackend/fullstackbackendapplication.java
+++ fullstack-backend-master/src/main/java/com/codewitharjun/fullstackbackend/fullstackbackendapplication.java
@@ -2,8 +2,9 @@ package com.codewitharjun.fullstackbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class fullstackbackendapplication {
 
 	public static void main(String[] args) {
